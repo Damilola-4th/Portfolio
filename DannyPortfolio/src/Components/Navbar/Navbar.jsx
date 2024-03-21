df8ff610d7bd73ad217f035771fada77b5cfb5b3
Damilola-4th <105187467+Damilola-4th@users.noreply.github.com>
2024-03-21 04:41:23 -0400

    Imported the Images neccesary for the Portfolio, and exported them in index.js

--- DannyPortfolio/src/Components/Navbar/Navbar.jsx
+++ DannyPortfolio/src/Components/Navbar/Navbar.jsx
@@ -1,16 +1,17 @@
-import React from 'react'
+import DanielLogo from '../../assets/Logo/DanielLogo.png'
 import "./Navbar.css"
 
 const Navbar = () => {
   return (
     <div className='navbar'>
-        <p className='Logo'>Daniel Oluwarotimi</p>
+        <img className='Logo' src ={DanielLogo} alt = "Daniel Oluwarotimi Logo" />
         <ul className= "nav-links">
             <li>Services</li>
             <li>Projects</li>
             <li>Resume</li>
+            <li>Experience</li>
+            <li>Interest</li>
             <li>About</li>
-            
         </ul>
 
         <p  id='Contact'>Contact Me</p>
---
+++ DannyPortfolio/src/assets/Index.js
@@ -0,0 +1,53 @@
+import logo from './Logo/DanielLogo.png'
+
+import Google from './Companies/Google.png'
+import Salesforce from './Companies/Salesforce.png'
+
+import Cooking from './Interest/Cooking.png'
+import Fitness from './Interest/Fitness.png'
+import Reading from './Interest/Reader.png'
+import TablesTennis from './Interest/TableTennis.png'
+import Pianist from './Interest/Pianist.png'
+
+import Javascript from './TechSkills/Javascript.png'
+import Express from './TechSkills/Expressjs.png'
+import HTML from './TechSkills/HTML.png'
+import CSS from './TechSkills/CSS.png'
+import Python from './TechSkills/Python.png'
+import Flask from './TechSkills/Flask.png'
+import PSQL from './TechSkills/PSQL.png'
+import React from './TechSkills/React.png'
+import Unity from './TechSkills/Unity.png'
+import Git from './TechSkills/Git.png'
+import Github from './TechSkills/Github.png'
+import Heroku from './TechSkills/Heroku.png'
+import GoogleCloud from './TechSkills/GoogleCloud.jpg'
+import Node from './TechSkills/Node.png'
+
+
+export {
+    logo,
+    Salesforce,
+    Google,
+    Javascript,
+    Flask,
+    HTML,
+    CSS,
+    React,
+    Express,
+    Python,
+    PSQL,
+    Unity,
+    Git,
+    Github,
+    Heroku,
+    GoogleCloud,
+    Node,
+    Cooking,
+    Reading,
+    Pianist,
+    TablesTennis,
+    Fitness
+
+
+}
